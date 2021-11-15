cf8f44c223cca78ea8c99fab5291b6916db6763e
gerrygralton <gerry@gralton.net>
2021-11-15 14:09:49 +0800

    get corners better

--- consistent_stl.jl
+++ consistent_stl.jl
@@ -6,11 +6,14 @@ function consistent_stl(input::String, output::String; len_scale::Real=1.)
 
     gmsh.merge(input)
 
-    gmsh.model.mesh.classifySurfaces(2, true, true)
+    angle = pi*0.3
+    gmsh.model.mesh.createTopology()
+    gmsh.model.mesh.classifySurfaces(angle, true, true)
     gmsh.model.mesh.createGeometry()
 
     gmsh.model.mesh.set_size(gmsh.model.getEntities(0), len_scale)
     gmsh.model.mesh.generate(2)
+    gmsh.model.mesh.optimize("Netgen")
 
     gmsh.write(output)
 
--- consistent_stl.py
+++ consistent_stl.py
@@ -1,5 +1,6 @@
 import gmsh
 import sys
+import math
 
 input = sys.argv[1]
 output = sys.argv[2]
@@ -12,11 +13,14 @@ gmsh.option.setNumber("General.NumThreads", 4)
 
 gmsh.merge(input)
 
-gmsh.model.mesh.classifySurfaces(2, True, True)
+angle = math.pi * 0.3
+gmsh.model.mesh.createTopology()
+gmsh.model.mesh.classifySurfaces(angle, True, True)
 gmsh.model.mesh.createGeometry()
 
 gmsh.model.mesh.set_size(gmsh.model.getEntities(0), len_scale)
 gmsh.model.mesh.generate(2)
+gmsh.model.mesh.optimize("Netgen")
 
 gmsh.write(output)
 
