de52d8ae2b3a11a092792c5d42bd6a27770decf1
gerrygralton <gerry@gralton.net>
2021-11-15 15:54:51 +0800

    Use argsparse instead of sys

--- consistent_stl.py
+++ consistent_stl.py
@@ -1,27 +1,33 @@
 import gmsh
-import sys
+import argparse
 import math
 
-input = sys.argv[1]
-output = sys.argv[2]
-len_scale = float(sys.argv[3])
+parser = argparse.ArgumentParser(description="Input arguments")
+parser.add_argument("input", metavar="input", type=str,
+                    help="Path to mesh")
+parser.add_argument("output", metavar="output", type=str,
+                    help="Output path")
+parser.add_argument("length", metavar="length", type=float,
+                    help="Characteristic length mesh")
+
+args = parser.parse_args()
 
 gmsh.initialize()
 gmsh.clear()
 
 gmsh.option.setNumber("General.NumThreads", 4)
 
-gmsh.merge(input)
+gmsh.merge(args.input)
 
 angle = math.pi * 0.3
 gmsh.model.mesh.createTopology()
 gmsh.model.mesh.classifySurfaces(angle, True, True)
 gmsh.model.mesh.createGeometry()
 
-gmsh.model.mesh.set_size(gmsh.model.getEntities(0), len_scale)
+gmsh.model.mesh.set_size(gmsh.model.getEntities(0), args.length)
 gmsh.model.mesh.generate(2)
 gmsh.model.mesh.optimize("Netgen")
 
-gmsh.write(output)
+gmsh.write(args.output)
 
 gmsh.clear()
